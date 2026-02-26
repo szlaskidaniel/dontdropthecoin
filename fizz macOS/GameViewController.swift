04dacca881107aa51be2513dcee0b692a7e4d19d
Daniel Szlaski <daniel.szlaski@icloud.com>
2026-02-26 16:39:22 +0100

    Initial Commit

---
+++ fizz macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  fizz macOS
+//
+//  Created by Daniel on 26/02/2026.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
